f04a4a4ba5abad81cff424d848a060221ae7ff07
Jitgitbit <rowowolf@gmail.com>
2020-05-09 02:10:40 +0200

    rxjs Subscription,    and OnDestroy  usage

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,13 +18,15 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
-    this.route.params.subscribe(                                // with params as an OBSERVABLE, subscribing to it will wait for an "in case that"  !!!
+    this.paramsSubscription = this.route.params.subscribe(       // with params as an OBSERVABLE, subscribing to it will wait for an "in case that"  !!!
       (params: Params) => {                                     // handy in case you are already in the same component, otherwise Angular will ignore the route change !!!
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     )
   }
-  ngOnDestroy(){}
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
+  }
 
 }
