321760b4550b1386da24118c6fe1e4938b946905
Jitgitbit <rowowolf@gmail.com>
2020-05-09 04:43:36 +0200

    Configuring the Handling of Query Parameters

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -21,7 +22,12 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(`===>> queryParams says what:`, this.route.snapshot.queryParams);
     console.log(`==> fragment says what:`, this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();                                // with subscribe this will allow you to react to changed queryParams (like params, Observables)
+
+    this.route.queryParams.subscribe(                          // with subscribe this will allow you to react to changed queryParams (like params, Observables)
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );                                
     this.route.fragment.subscribe();                                  // as above for changed fragment (like params, other Observables), ensuring not to miss data !!!   
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
